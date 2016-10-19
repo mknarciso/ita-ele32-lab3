346dd22d94f9d5e8eb8c47fe136af7cd8c06be87
mknarciso <murilokn@gmail.com>
2016-10-19 17:34:53 +0000

    Scramble

--- src/main/java/lab3/Codificador.java
+++ src/main/java/lab3/Codificador.java
@@ -3,13 +3,16 @@ package lab3;
 
 
 public class Codificador {
+    private static int[] header = {0,0,0,1, 1,0,1,0, 1,1,0,0, 1,1,1,1, 1,1,1,1, 1,1,0,0, 0,0,0,1, 1,1,0,1};
     private int[] bitsIn = new int[1000];
     private int[] bits2 = new int[2000];
     private int[] bits3 = new int[2032];
+    private int[] bits4 = new int[2032];
     private int[] cs = {0,0,0,0,0,0};
+    private int[] ss = {1,1,1,1, 1,1,1,1};
+    
     public void convolute(){
-        int i = 0;
-        while (i<1000||cs[0]==0||cs[1]==0||cs[2]==0||cs[3]==0||cs[4]==0||cs[5]==0){
+        for(int i =0; i<1000; i++){
             bits2[2*i]   =(bitsIn[i]+cs[0]+cs[1]+cs[2]+cs[5])%2;
             bits2[2*i+1] =(bitsIn[i]+cs[1]+cs[2]+cs[4]+cs[5])%2;
             cs[0] = bitsIn[i];
@@ -18,6 +21,30 @@ public class Codificador {
             cs[3] = cs[2];
             cs[4] = cs[3];
             cs[5] = cs[4];
+        }
+    }
+    
+    public void addHeader(){
+        for(int i=0;i<2000;i++){
+            bits3[i]=bits2[i];
+        }
+        for(int i=0;i<32;i++){
+            bits3[2000+i]=header[i];
+        }
+    }
+    
+    public void scramble(){
+        for(int i=0;i<2032;i++){
+            bits4[i]=(bits3[i]+ss[7])%2;
+            ss[0]=(ss[0]+ss[2]+ss[4]+ss[7])%2;
+            ss[1]=ss[0];
+            ss[2]=ss[1];
+            ss[3]=ss[1];
+            ss[4]=ss[1];
+            ss[5]=ss[1];
+            ss[6]=ss[1];
+            ss[7]=ss[1];
+            ss[2]=ss[1];
         }
     }
 }

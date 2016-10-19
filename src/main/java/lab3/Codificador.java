f6a93077c0c5ccbbcbb13ed874df85a9e35741f8
mknarciso <murilokn@gmail.com>
2016-10-19 17:49:03 +0000

    Juntado

--- src/main/java/lab3/Codificador.java
+++ src/main/java/lab3/Codificador.java
@@ -1,8 +1,10 @@
 package lab3;
-
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Codificador {
+	public static String texto;
+	public static int posicao = 0;
     private static int[] header = {0,0,0,1, 1,0,1,0, 1,1,0,0, 1,1,1,1, 1,1,1,1, 1,1,0,0, 0,0,0,1, 1,1,0,1};
     private int[] bitsIn = new int[1000];
     private int[] bits2 = new int[2000];
@@ -11,6 +13,37 @@ public class Codificador {
     private int[] cs = {0,0,0,0,0,0};
     private int[] ss = {1,1,1,1, 1,1,1,1};
     
+	public static void lerTexto(String file){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		       texto = texto + line;
+		    }
+		    br.close();
+		}
+		catch (Exception e){
+			System.out.println("Deu ruim!");
+		}
+	}
+	
+	public void gerarBitsIn (){
+		char aux;
+		int a = texto.length();
+		if(a < posicao + 1000){
+			for(int i = 0; i < posicao + 10000- a; i ++){
+				texto = texto+ "0";
+			}
+		}
+		for (int i = 0; i < 1000; i ++){
+			aux = texto.charAt(posicao + i);
+			if(aux == '1')
+				bitsIn[i] = 1;
+			else bitsIn[i] = 0;
+		}
+		posicao = posicao + 1000;
+	}
+	
     public void convolute(){
         for(int i =0; i<1000; i++){
             bits2[2*i]   =(bitsIn[i]+cs[0]+cs[1]+cs[2]+cs[5])%2;
@@ -46,5 +79,19 @@ public class Codificador {
             ss[7]=ss[1];
             ss[2]=ss[1];
         }
+    }
+
+	 /*public void armazenaEmHexadecimal (){
+		 int a;
+		 String 
+		 for (int i = 0; i <508; i ++){
+			 a = 1000*bits4[i] + 100* bits4[i+1] + 10*bits4[i+2] + bits4[i+4];
+			 
+		 }
+		 
+	 }*/
+    
+    public void run(){
+        
     }
 }
